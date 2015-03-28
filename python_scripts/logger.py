89ce91019c2f3306acb32399017cebdf6a44c995
twelvetosix <seanschellinger@gmail.com>
2015-03-28 13:02:08 -0400

    Created laucnher scripts directory Modified logger.py for new table and to use float

--- python_scripts/logger.py
+++ python_scripts/logger.py
@@ -12,8 +12,6 @@ db = MySQLdb.connect(host="localhost",
 					  
 cur = db.cursor()
 
-
-
 ser = serial.Serial('/dev/ttyACM0',9600)
 ser.open()
 ser.readline()
@@ -22,8 +20,13 @@ while 1:
 	i = datetime.now()
 	data = ser.readline()
 	print i.strftime('%Y-%m-%d %H:%M:%S') + " " + data
-	cur.execute("INSERT INTO TempLog (Date, Temp) VALUES ('%s', '%s')" % (i.strftime('%Y-%m-%d %H:%M:%S'), data))
-	db.commit()
+	if data <= 0:
+		# do nothing
+		print "ERROR"	
+	else:		
+		floatTemp = float(data)
+		cur.execute("INSERT INTO LogTable (Date, Temp) VALUES ('%s', '%f')" % (i.strftime('%Y-%m-%d %H:%M:%S'), floatTemp))
+		db.commit()
 	time.sleep(5)
 	
 
--- python_scripts/tempsensor.py
+++ python_scripts/tempsensor.py
@@ -1,6 +1,17 @@
 import time
+from datetime import datetime
+import time
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+					  passwd="skelnjr3",
+					  db="raspberrypi")
+					  
+cur = db.cursor()
 
 while 1:
+	i = datetime.now()
 	tempfile = open("/sys/bus/w1/devices/28-0014156038ff/w1_slave")
     	thetext = tempfile.read()
 	tempfile.close()
@@ -10,6 +21,10 @@ while 1:
 	temp = temp * 9
 	temp = temp / 5
 	temp = temp + 32
-	print temp
+#	print i.strftime('%Y-%m-%d %H:%M:%S')
+#	print temp
+	cur.execute("INSERT INTO TempLog (Date, Temp) VALUES ('%s', '%s')" % (i.strftime('%Y-%m-%d %H:%M:%S'), temp))
+	db.commit()
+	time.sleep(5)
 
-	time.sleep(3)
+	
